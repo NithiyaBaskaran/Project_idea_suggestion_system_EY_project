7f8b60acbd1cbdb2966aa68c873c0d81a55a533c
NithiyaBaskaran <nithiya102005@gmail.com>
2026-02-20 11:05:17 +0530

    Add files via upload

---
+++ itactuallyworks/users/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+app_name = 'users'
+urlpatterns = [
+    path('', views.homepage1, name='homepage1'),      # homepage1
+    path('signup/', views.signup, name='signup'),     # signup  
+    path('login/', views.login_view, name='login'),   # login
+    path('forgotpassword/', views.forgotpassword_view, name='forgotpassword'),
+    path('homepage/', views.homepage, name='homepage'), # homepage
+    path('domainselection/', views.domain, name='domain'),
+    path('projects/', views.projects, name='projects'),
+    path('projectdetail/', views.projectdetail, name='projectdetail'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('bookmark/<int:project_id>/', views.bookmark, name='bookmark'),
+    path('feedback/',views.feedback , name='feedback'),
+    path('logout/', views.logout_view, name='logout'),  # logout
+]
+
+
